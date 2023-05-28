2af20bfb49b7549bb12f09a09d98a66ee32c75e5
kwingston <mahesh.mummbrekar@gmail.com>
2023-05-28 08:31:54 +0530

    new java program

---
+++ java/app.js
@@ -0,0 +1,8 @@
+ let age= 32;
+let grettingsText = 'hi this is the error';
+alert(grettingsText);
+alert(grettingsText);
+grettingsText='his this is error 65';
+alert(grettingsText);
+alert(age)
+alert(age)
