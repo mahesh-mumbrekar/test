9a915a6ba0970b8b639ea340ef1477a5f2a01116
kwingston <mahesh.mummbrekar@gmail.com>
2023-05-30 08:43:57 +0530

    javascript function and basic command

--- java/app.js
+++
@@ -1,8 +0,0 @@
- let age= 32;
-let grettingsText = 'hi this is the error';
-alert(grettingsText);
-alert(grettingsText);
-grettingsText='his this is error 65';
-alert(grettingsText);
-alert(age)
-alert(age)
